866f412a7ad4976d6206cd3d21a3a89afd17865c
Uddeshya <uddeshya.vishwakarma@shiprocket.com>
2025-05-12 13:11:24 +0530

    health check URL added

--- src/transports/sse.ts
+++ src/transports/sse.ts
@@ -1,6 +1,6 @@
 import { mcpServer } from "@/mcp/index";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { connectionsBySessionId } from "@/mcp/connections";
 import "dotenv/config";
 
@@ -9,6 +9,13 @@ const PORT = process.env.APP_PORT;
 const app = express();
 
 app.use(express.json());
+
+app.get("/health-check", async (req, res) => {
+  res.json({
+    success: true,
+    message: "All is well!!!",
+  });
+});
 
 app.get("/sse", async (req, res) => {
   const sellerToken = req.query?.st as string;
@@ -38,5 +45,50 @@ app.post("/messages", async (req, res) => {
 
   await transport.handlePostMessage(req, res, req.body);
 });
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Looking for something?",
+  });
+});
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (
+    err instanceof SyntaxError &&
+    "status" in err &&
+    err.status === 400 &&
+    "body" in err
+  ) {
+    res.status(400).json({ success: false, message: "Invalid JSON payload" });
+    return;
+  }
+
+  console.error(`Error occurred during request: ${err.stack}`);
+
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
+// Catch synchronous exceptions that are not caught by Express
+process.on("uncaughtException", (error: Error) => {
+  console.error(`Uncaught Exception: ${error.stack}`);
+  process.exit(1);
+});
+
+// Catch unhandled promise rejections
+process.on(
+  "unhandledRejection",
+  (reason: unknown, promise: Promise<unknown>) => {
+    console.error(
+      `Unhandled Rejection at: ${promise} reason: ${
+        reason instanceof Error ? reason.stack : reason
+      }`
+    );
+    process.exit(1);
+  }
+);
 
 app.listen(PORT, () => console.log(`MCP Server listening on port ${PORT}...`));
